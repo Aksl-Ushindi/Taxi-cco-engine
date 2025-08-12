587ad957356e0639eae13facd3fddfcdfa7ee4c1
Aksl-Ushindi <ton_email_github@exemple.com>
2025-08-12 17:27:43 +0200

    Deploy sync 12.08.2025 17:27:43,14

---
+++ deploy.bat
@@ -0,0 +1,33 @@
+@echo off
+set "REPO_URL=https://github.com/Aksl-Ushindi/Taxi-cco-engine.git"
+set "REPO_DIR=%USERPROFILE%\Desktop\GitHub\Taxi-cco-engine"
+set "SOURCE_DIR=C:\Users\Darboux\Desktop\API\taxi cco engine"
+set "BR=main"
+
+where git >nul 2>&1 || (echo Git manquant & pause & exit /b 1)
+
+if not exist "%REPO_DIR%\.git" (
+  if not exist "%REPO_DIR%" mkdir "%REPO_DIR%"
+  git clone "%REPO_URL%" "%REPO_DIR%" || (echo Clone KO & pause & exit /b 1)
+)
+
+pushd "%REPO_DIR%"
+git checkout "%BR%" 2>nul & git pull origin "%BR%"
+popd
+
+robocopy "%SOURCE_DIR%" "%REPO_DIR%" /E /R:1 /W:1 ^
+ /XD .git node_modules .next dist build ^
+ /XF Thumbs.db desktop.ini .DS_Store >nul
+
+REM (facultatif) URL racine:
+copy /Y "%REPO_DIR%\taxi-cco-engine-0.9.html" "%REPO_DIR%\index.html" >nul
+
+pushd "%REPO_DIR%"
+git add -A
+git diff --cached --quiet && (echo Rien a pousser & popd & goto :end)
+git commit -m "Deploy sync %date% %time%"
+git push origin "%BR%"
+popd
+:end
+echo OK
+pause
